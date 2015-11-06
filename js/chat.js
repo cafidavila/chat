ab6b07c866cb8c750e339c0929d98535d01c7d50
cafidavila <cafi_jb@hotmail.com>
2015-11-06 14:14:13 -0500

    cambio

--- js/chat.js
+++ js/chat.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
 			$("#go").click(function(e){
-				e.preventDefault();  //previene el evento click , q se detenga
+				e.preventDefault();  //previene el evento click , que se detenga
 				var texto = $("#my-input").val();
-				var miPrimerMensaje = $("<p></p>").text(texto).addClass("bg-primary"); //etiquetas al vuelo
+				var color = $("#color").val()
+				var miPrimerMensaje = $("<p></p>").text(texto).addClass("alert-"+color); //etiquetas al vuelo
 				$(".resultado").append(miPrimerMensaje);
 			}); 
 
 		});
 
 
-$(document).ready(function(){
-			$("#red").click(function(e){
-				e.preventDefault();  //previene el evento click , q se detenga
-				var miPrimerMensaje = $("<p></p>").text(texto).addClass("bg-danger"); //etiquetas al vuelo
-				$(".resultado").append(miPrimerMensaje);
-			}); 
 
-		});
 
 
